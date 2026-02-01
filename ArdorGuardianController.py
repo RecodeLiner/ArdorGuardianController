d077bd4a4c7fd866cb0a35b8f0a6b316eea8df83
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2026-02-01 21:30:40 +0300

    Unificate protocol

--- ArdorGuardianController.py
+++ ArdorGuardianController.py
@@ -46,77 +46,8 @@ class ArdorGuardianController:
         time.sleep(0.05)
         self.device.write([0x04, 0x01, 0x00, 0x01] + [0x00] * 60)
         time.sleep(0.05)
-        
-    def set_static_color(self, r, g, b, brightness=4, save_to_memory=False):
-        if not self.device:
-            return
 
-        BRIGHTNESS_CODES = [0xd0, 0xd3, 0xd7, 0xdb, 0xdf]
-
-        if not 0 <= brightness <= 4:
-            brightness = 2
-
-        code = BRIGHTNESS_CODES[brightness]
-
-        cmd = 0x27 if not save_to_memory else 0x37
-
-        packet = [
-            0x04, cmd, 0x01, 0x06, 0x22,
-            0x00, 0x00, 0x00, 0x00,
-            0x06, brightness, 0x02, 0x00, 0x00,
-            r, g, b
-        ]
-
-        packet += [0x00] * (64 - len(packet))
-        self.device.write(packet)
-
-    def set_breath_mode(self, r, g, b, brightness=4, speed=2, save_to_memory=False):
-        if not self.device:
-            return
-        
-        if not 0 <= brightness <= 4:
-            brightness = 4
-        if not 0 <= speed <= 4:
-            speed = 2
-        
-        cmd = 0x32 if not save_to_memory else 0x42
-        
-        packet = [
-            0x04, cmd, 0x01, 0x06,
-            0x22, 0x00, 0x00, 0x00,
-            0x00, 0x05, brightness, speed,
-            0x00, 0x00, r, g, b
-        ]
-        packet += [0x00] * (64 - len(packet))
-        
-        self.device.write(packet)
-    
-    def set_waterfall_mode(self, r, g, b, brightness=4, speed=2, clockwise=True, save_to_memory=False):
-        if not self.device:
-            return
-        
-        if not 0 <= brightness <= 4:
-            brightness = 4
-        if not 0 <= speed <= 4:
-            speed = 2
-        
-        cmd = 0x31 if clockwise else 0x30
-        if save_to_memory:
-            cmd += 0x10  # 0x41 or 0x40
-        
-        direction = 0x01 if clockwise else 0x00
-        
-        packet = [
-            0x04, cmd, 0x01, 0x06,
-            0x22, 0x00, 0x00, 0x00,
-            0x00, 0x03, brightness, speed,  # 0x03 - waterfall
-            direction, 0x00, r, g, b
-        ]
-        packet += [0x00] * (64 - len(packet))
-        
-        self.device.write(packet)
-
-    def set_arrow_mode(self, r, g, b, brightness=4, speed=0, save_to_memory=False):
+    def set_effect(self, effect_type, r, g, b, brightness=4, speed=0, isClockwise=False, save_to_memory=False):
         if not self.device:
             return
         
@@ -125,17 +56,32 @@ class ArdorGuardianController:
         if not 0 <= speed <= 4:
             speed = 0
         
-        cmd = 0xb8 if not save_to_memory else 0xc8
+        cmd = 0x27 if not save_to_memory else 0x37
         
         packet = [
-            0x04, cmd, 0x02, 0x06,
+            0x04, cmd, 0x01, 0x06,
             0x22, 0x00, 0x00, 0x00,
-            0x00, 0x09, brightness, speed,
-            0x01, 0x00, r, g, b
+            0x00, effect_type, brightness, speed,
+            isClockwise, 0x00, r, g, b
         ]
         packet += [0x00] * (64 - len(packet))
         
         self.device.write(packet)
+        
+    def set_static_color(self, r, g, b, brightness=4, save_to_memory=False):
+        self.set_effect(0x06, r, g, b, brightness, speed=0x0, isClockwise=False, save_to_memory=save_to_memory)
+
+    def set_arrow_mode(self, r, g, b, brightness=4, speed=0, save_to_memory=False):
+        self.set_effect(0x09, r, g, b, brightness, speed, isClockwise=False, save_to_memory=save_to_memory)
+
+    def set_waterfall_mode(self, r, g, b, brightness=4, speed=0, isClockwise=False, save_to_memory=False):
+        self.set_effect(0x03, r, g, b, brightness, speed, isClockwise=isClockwise, save_to_memory=save_to_memory)
+
+    def set_breath_mode(self, r, g, b, brightness=4, speed=0, save_to_memory=False):
+        self.set_effect(0x05, r, g, b, brightness, speed, isClockwise=False, save_to_memory=save_to_memory)
+
+    def set_wave_mode(self, r, g, b, brightness=4, speed=0, save_to_memory=False):
+        self.set_effect(0x08, r, g, b, brightness, speed, isClockwise=False, save_to_memory=save_to_memory)
 
     def close(self):
         if self.device:
